fba4984d2e5616110662416f449f18db3ab68049
Adytia855 <maditya855@gmail.com>
2025-06-16 18:57:41 +0700

    adding profile section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Profile from './components/Profile'
 
 const App = () => {
   return (
     <main id="body" className="bg-black noise min-h-screen text-white">
       <Navbar />
+      <section id="profileContainer" class="pt-26 flex flex-col lg:flex-row gap-6 min-h-screen space-y-8 px-4 md:px-28 lg:px-24 xl:px-60">
+        <Profile />
+      </section>
     </main>
   )
 }
